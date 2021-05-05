87a4c7d90431b4b208b5f3daff8b4640a85faf10
xprmnts <abhinay@outlook.com>
2021-05-05 18:13:40 -0400

    addeded next auth config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         mongodburl:
-            'mongodb+srv://admin:fto1ByZoJiCXn7Se@cluster0.wnvfs.mongodb.net/mastermind?retryWrites=true&w=majority'
+            'mongodb+srv://admin:fto1ByZoJiCXn7Se@cluster0.wnvfs.mongodb.net/mastermind?retryWrites=true&w=majority',
+        NEXTAUTH_URL: 'https://abhinay-mastermind-evolved.herokuapp.com'
     }
 };
