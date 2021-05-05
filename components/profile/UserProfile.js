845a35e5caed0e5557edb3d03cd96a8c9240d4f7
xprmnts <abhinay@outlook.com>
2021-05-05 12:52:01 -0400

    added placeholder profile page

---
+++ components/profile/UserProfile.js
@@ -0,0 +1,11 @@
+function UserProfile() {
+    // TODO: Redirect away if NOT auth
+
+    return (
+        <section>
+            <h1>Your User Profile</h1>
+        </section>
+    );
+}
+
+export default UserProfile;
--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <Layout className='layout'>
             <MainHeader />
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
         </Layout>
     );
 }
